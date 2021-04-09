a341e6be0865f1e561ccdab9e2153fabc526479f
menaSami <mena5sami@gmail.com>
2021-04-10 00:53:59 +0200

    START THE STYLES

--- vendor/js/script.js
+++ vendor/js/script.js
@@ -1,8 +1,24 @@
 console.clear();
 
 var app = new Vue({
-  el: '#app',
+  el: 'main#app',
   data: {
-    message: 'Hello from Vue!'
-  }
+    message: 'Hello from Vue!',
+    colors: {
+      trans: 'transparent',
+      white: getComputedStyle(document.documentElement).getPropertyValue("--white-color"),
+      black: getComputedStyle(document.documentElement).getPropertyValue("--black-color"),
+      pink: getComputedStyle(document.documentElement).getPropertyValue("--pink-color"),
+      blue: getComputedStyle(document.documentElement).getPropertyValue("--blue-color"),
+      gray: getComputedStyle(document.documentElement).getPropertyValue("--gray-color"),
+      green: getComputedStyle(document.documentElement).getPropertyValue("--green-color"),
+      yellow: getComputedStyle(document.documentElement).getPropertyValue("--yellow-color"),
+      red: getComputedStyle(document.documentElement).getPropertyValue("--red-color"),
+      
+      link: getComputedStyle(document.documentElement).getPropertyValue("--link-color"),
+      main: getComputedStyle(document.documentElement).getPropertyValue("--main-color"),
+    },
+  },
+  methods: {
+  },
 });
